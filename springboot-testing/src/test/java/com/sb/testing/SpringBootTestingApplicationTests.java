7556f7df1ca83babb14b5d98a9ede2e1ee1ec8f0
RajasekharReddy.Kolli <rajasekharreddy.kolli@cognizant.com>
2021-06-26 19:10:54 +0530

    firstcommit

---
+++ springboot-testing/src/test/java/com/sb/testing/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
